01959ccd5986769c0e619df01979b14eaab65c90
damian hii <dhii@uci.edu>
2018-08-29 16:28:11 -0700

    attempting to parse customer.csv data into table

---
+++ testd3.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const d3 = require('d3');
+
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', function (req, res){
+	res.sendFile(path.join(__dirname, 'testd3.html'));
+	//console.log("hello");
+});
+
+app.listen(8080);
